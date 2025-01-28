a5a7736ca7663d1792e37c540874a170cdea8ce8
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-28 20:23:46 +0530

    update

--- src/app/components/Journey.tsx
+++ src/app/components/Journey.tsx
@@ -6,11 +6,11 @@ const Journey = () => {
     { year: '2018', description: 'Started learning C++ on my own through YouTube videos.' },
     { year: '2019', description: 'Got introduced to hacking, played with VMs, explored dw.' },
     { year: '2020', description: 'Was obsessed with making hacking tools like keyloggers and ransomware.' },
-    { year: '2022', description: 'Got bored of hacking and development and explored different paths (biggest mistake).' },
+    { year: '2022', description: 'Got bored of hacking/development and explored different paths (biggest mistake).' },
     { year: '2023', description: 'Ended up choosing a useless degree to pursue.<br/>(I did not choose CS because of the AI hype; I was too scared that AI would make coding obsolete).' },
     { year: '2024', description: 'Started getting back into coding.' },
-    { year: '2025', description: 'First 6 months : </br> Focusing on dev and DSA/CP,want to be very good at it. </br>later half : </br> will be geting inot AI/ML' },
-    { year: '2026', description: 'Will graduade. Next aiming to do a MS in CS while working remote jobs' },
+    { year: '2025', description: 'First 6 months : </br> Focusing on dev and DSA/CP,want to be very good at it. </br>later half : </br> will be geting into AI/ML' },
+    { year: '2026', description: 'Will graduade. Next aiming to do a MS in CS while working a remote jobs' },
   ];
 
   return (
