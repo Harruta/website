2d770e3342896d42d9a1687da14478d39773ed59
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-28 13:45:47 +0530

    navber items

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,6 +4,15 @@ const Navbar:React.FC = () => {
     const navItems: string[] = ['Projects', 'Blogs','Notes']
   return (
     <nav className='flex items-center space-x-9 bg-black p-4'>
+      {navItems.map((item,index) => (
+        <a
+        key={index}
+        href={`#${item.toLowerCase()}`}
+        className='text-white hover:text-blue-500 transition-colors'
+        >
+          {item}
+        </a>
+      ))}
 
     </nav>
   )
