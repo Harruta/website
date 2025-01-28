bf6e20e37bf9808f32f6de7e0e70b0b886086568
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-28 15:23:17 +0530

    uadate

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -6,8 +6,10 @@ import { ModeToggle } from '../components/ui/ModeToggle'
 export default function Homepage() {
   return (
     <div className='flex justify-start items-center mb-8 w-full pl-96'>
+      <div className='flex justify-betwen items-center gap-60'>
       <Navbar />
       <ModeToggle/>
+      </div>
     </div>
   )
 }
