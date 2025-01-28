1abfd25f11c715ef2291030f980c83179c29bad3
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-28 15:04:52 +0530

    added theam button for dark and light mdoe

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { ModeToggle } from '../components/ui/ModeToggle'
 
 export default function Homepage() {
   return (
     <div className='flex justify-start items-center mb-8 w-full pl-96'>
       <Navbar />
+      <ModeToggle/>
     </div>
   )
 }
