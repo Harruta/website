7a77245c575ec9ee6f85e6ac13adee3a5783e64e
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-28 20:55:55 +0530

    update

--- src/app/components/Journey.tsx
+++ src/app/components/Journey.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Journey = () => {
   const milestones = [
-    { year: '2017', description: 'Got my very first laptop.' },
-    { year: '2018', description: 'Started learning C++ on my own through YouTube videos.' },
-    { year: '2019', description: 'Got introduced to hacking, played with VMs, explored dw.' },
-    { year: '2020', description: 'Was obsessed with making hacking tools like keyloggers and ransomware.' },
-    { year: '2022', description: 'Got bored of hacking/development and explored different paths (biggest mistake).' },
+    { year: '2016', description: 'Got my very first laptop.' },
+    { year: '2017', description: 'Started learning C++ on my own through YouTube videos.' },
+    { year: '2018', description: 'Got introduced to hacking, played with VMs, explored dw.' },
+    { year: '2019', description: 'Was obsessed with making hacking tools like keyloggers and ransomware.' },
+    { year: '2020', description: 'Got bored of hacking/development. explored different paths (biggest mistake).' },
+    { year: '2021', description: 'Was on a different path completely forgot about coding.' },
+    { year: '2022', description: 'passed 12th and took a gap year to prepare for studying abroad (got rejected).' },
     { year: '2023', description: 'Ended up choosing a useless degree to pursue.<br/>(I did not choose CS because of the AI hype; I was too scared that AI would make coding obsolete).' },
     { year: '2024', description: 'Started getting back into coding.' },
     { year: '2025', description: 'First 6 months : </br> Focusing on dev and DSA/CP,want to be very good at it. </br>later half : </br> will be geting into AI/ML' },
