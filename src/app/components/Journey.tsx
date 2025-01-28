9037566ee223bbcaa14483a82e9fe2c37ecf6511
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-28 21:00:05 +0530

    update

--- src/app/components/Journey.tsx
+++ src/app/components/Journey.tsx
@@ -12,7 +12,7 @@ const Journey = () => {
     { year: '2023', description: 'Ended up choosing a useless degree to pursue.<br/>(I did not choose CS because of the AI hype; I was too scared that AI would make coding obsolete).' },
     { year: '2024', description: 'Started getting back into coding.' },
     { year: '2025', description: 'First 6 months : </br> Focusing on dev and DSA/CP,want to be very good at it. </br>later half : </br> will be geting into AI/ML' },
-    { year: '2026', description: 'Will graduade. Next aiming to do a MS in CS while working a remote jobs' },
+    { year: '2026', description: 'Will graduade. Next aiming to do a MS in CS while working a remote job' },
   ];
 
   return (
