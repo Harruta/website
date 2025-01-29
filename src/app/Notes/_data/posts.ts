fa1571e35d89da57c7580a0559f012b27d63c907
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-29 23:41:38 +0530

    notes

--- src/app/Notes/_data/posts.ts
+++ src/app/Notes/_data/posts.ts
@@ -11,14 +11,14 @@ export const notes: NotesTypes[] = [
     {
         id: 2, 
         title: "AI/ML notes",
-        description: "PyTorch code snippets that I've used & learned from for quick reference.",
+        description: "Everrythng related to AI/ML.",
         lastUpdated: "January 29, 2025",
         href: "/notes/AI/ML"
     },
     {
         id: 3,
         title: "Books",
-        description: "Notes and summaries from the books I'm reading to learn ML,Math and coding.",
+        description: "AI, Math and coding Book Summeries, will have their important concepts in here for quick reference.",
         lastUpdated: "January 29, 2025",
         href: "/notes/books"
     }
